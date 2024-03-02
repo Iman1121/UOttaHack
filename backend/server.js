b73e8f8eecbb6d6e5c86ccac160e28b18b37173c
Iman1121 <imanullah1112@gmail.com>
2024-03-02 02:25:52 -0500

    update

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ mssg: "works" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("listening on port", process.env.PORT);
+});
