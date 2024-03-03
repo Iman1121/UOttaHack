bc1c35d565af3304f2d705197773fc1ae6c6db48
Brandon Le <brandonle2@outlook.com>
2024-03-03 05:03:34 -0500

    a lot

--- app/src/Message.js
+++ app/src/Message.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
 const Message = ({ message }) => {
-  return (
-    <div className="message-container" style={{ backgroundColor: 'rgb(204, 113, 120)', borderRadius: '10px', display: 'flex', flexDirection: 'column', marginTop: '10px' }}>
-      <div className="user-id" style={{paddingTop:"5px", paddingLeft:"10px",fontSize: '15px', color: 'white', margin: '0'}}>
-        <p style={{margin: '0'}}>{message.userName}:</p>
+  if (message.pictureURI === "") {
+    return (
+      <div className="message-container" style={{ backgroundColor: 'rgb(204, 113, 120)', borderRadius: '10px', display: 'flex', flexDirection: 'column', marginTop: '10px' }}>
+        <div className="user-id" style={{ paddingTop: "5px", paddingLeft: "10px", fontSize: '15px', color: 'white', margin: '0' }}>
+          <p style={{ margin: '0' }}>{message.userName}:</p>
+        </div>
+        <div className="message-text">
+          <h3 style={{ color: 'white', margin: '0', paddingBottom: "8px", paddingLeft: "10px" }}>{message.text}</h3>
+        </div>
       </div>
-      <div className="message-text">
-        <h3 style={{color: 'white', margin: '0',paddingBottom:"8px", paddingLeft:"10px" }}>{message.text}</h3>
+    );
+  } else {
+    return (
+      <div className="message-container" style={{ backgroundColor: 'rgb(204, 113, 120)', borderRadius: '10px', display: 'flex', flexDirection: 'column', marginTop: '10px' }}>
+        <div className="user-id" style={{ paddingTop: "5px", paddingLeft: "10px", paddingRight: "10px", fontSize: '15px', color: 'white', margin: '0' }}>
+          <p style={{ margin: '0' }}>{message.userName}:</p>
+        </div>
+        <div className="message-image" style={{ maxWidth: '100%', maxHeight: '200px', overflow: 'hidden', padding: '10px' }}>
+          <img src={message.pictureURI} alt="Message Image" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Message;
--- app/src/noteComponent.js
+++ app/src/noteComponent.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-const noteComponent = ({ note }) => {
-
-  return (
-    <div className="note-container" style={{ backgroundColor: 'rgb(204, 113, 120)', borderRadius: '10px', display: 'flex', flexDirection: 'column', marginTop: '10px' }}>
-
-      <div className="note-text">
-        <h3 style={{color: 'white', margin: '0',paddingBottom:"8px", paddingLeft:"10px" }}>{note.response}</h3>
+const NoteComponent = ({ note }) => {
+  if (note.pictureURI === "") {
+    return (
+      <div className="note-container" style={{ backgroundColor: 'rgb(204, 113, 120)', borderRadius: '10px', display: 'flex', flexDirection: 'column', marginTop: '10px' }}>
+        <div className="note-text">
+          <h3 style={{ color: 'white', margin: '0', paddingBottom: "8px", paddingTop: "2px", paddingLeft: "10px" }}>{note.response}</h3>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="note-container" style={{ backgroundColor: 'rgb(204, 113, 120)', borderRadius: '10px', display: 'flex', flexDirection: 'column', marginTop: '10px' }}>
+        <div className="note-image" style={{ maxWidth: '100%', maxHeight: '200px', overflow: 'hidden', padding: '10px' }}>
+          <img src={note.pictureURI} alt="Message Image" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+        </div>
+      </div>
+    );
+  }
 };
 
-export default noteComponent;
+export default NoteComponent;
