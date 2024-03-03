8fb64e70ca4d4cdf6637ff2713f1e307165566f6
Hassan Ali <thehassanali2@gmail.com>
2024-03-03 01:41:26 -0500

    Fixed weird classname error

--- app/src/TextForm.js
+++ app/src/TextForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import * as mdb from 'mdb-ui-kit'; // lib
 const TextForm = ({url, updateMessages} ) => {
   const [inputValue, setInputValue] = useState('');
 
