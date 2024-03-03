6963d808bb39335e649354f1a35ab6fde8da3311
Brandon Le <brandonle2@outlook.com>
2024-03-03 02:29:15 -0500

    update for iman

---
+++ app/src/TextFormNotes.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+const TextFormNotes = ({url, updateMessages} ) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    try {
+      console.log(url);
+      await axios.post('http://localhost:4000/api/notes',{msgId:2, lecId: 50, response: inputValue});
+      updateMessages({ text: inputValue }); // Trigger message update in parent component
+      setInputValue('');
+    } catch (error) {
+      console.error('Error submitting message:', error);
+      setInputValue('');
+    }
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <input 
+        type="text" 
+        value={inputValue} 
+        onChange={handleChange} 
+        placeholder="" 
+        style={{ marginRight: '10px', flex: 1, outlineColor: 'rgb(137, 2, 62)' }}
+      />
+       <button type="submit" style={{ backgroundColor: 'rgb(204,113,120)', color: 'white', border: 'none', borderRadius: '5px', padding: '8px 16px' }}>Submit</button>
+    </form>
+  );
+};
+
+export default TextFormNotes;
